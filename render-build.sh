26ca3be4c2edf7f8477507557f31422d9f3cd969
huiseok06 <huiseok2006@gmail.com>
2025-08-24 15:33:58 +0900

    chore: Render build script (ffmpeg/python) + requirements

---
+++ render-build.sh
@@ -0,0 +1,6 @@
+﻿#!/usr/bin/env bash
+set -e
+apt-get update
+apt-get install -y --no-install-recommends ffmpeg python3 python3-pip
+pip3 install --no-cache-dir -r requirements.txt
+npm ci --omit=dev || npm i
